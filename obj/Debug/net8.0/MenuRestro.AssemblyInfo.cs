58540ba2db39e8be1e74190e0e78eb119241e7ca
Abbdulazizbek2 <abdulazizbek2000boy@gmail.com>
2024-10-28 09:28:56 +0500

    some changes

--- obj/Debug/net8.0/MenuRestro.AssemblyInfo.cs
+++ obj/Debug/net8.0/MenuRestro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MenuRestro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc762e6207b0cfdc5c5ea9a54a8618c95520d47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5391ee7bd17c5bb5bbc73be1d5f8e91b51701a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MenuRestro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MenuRestro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
